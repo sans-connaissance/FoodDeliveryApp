dd51fcc8cbc2da2ca47dfb31e932276fda992dfa
sans-connaissance <davemalicke@gmail.com>
2021-09-06 21:35:19 -0400

    Created food data model and json decoder

---
+++ FoodDeliveryApp/Model/Food.swift
@@ -0,0 +1,26 @@
+//
+//  Food.swift
+//  FoodDeliveryApp
+//
+//  Created by David Malicke on 9/6/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Food: Hashable, Codable, Identifiable {
+    
+    var id: Int
+    var name: String
+    var image: String
+    var kitchen: Kitchen
+    var description: String
+    
+    enum Kitchen: String, CaseIterable, Codable, Hashable {
+        
+        case italian = "Italian"
+        case chinese = "Chinese"
+    }
+    
+    
+}
